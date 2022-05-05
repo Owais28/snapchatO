9a6abd52b8677074e656fc13c981fc320f7b1d59
Owais Athar <69184167+Owais28@users.noreply.github.com>
2022-05-05 12:48:11 +0530

    dsd

--- src/index.js
+++ src/index.js
@@ -4,52 +4,78 @@ import ReactDOM from 'react-dom';
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 // Components
-import Navbar from "./components/Navbar";
-import MainContent from "./components/MainContent";
-import Card from "./components/Card";
-import Map from "./components/Map";
-import data from "./data"
-import quotes from "./quotes";
-import posts from "./data/posts";
+// import Navbar from "./components/Navbar";
+// import MainContent from "./components/MainContent";
+// import Card from "./components/Card";
+// import Map from "./components/Map";
+// import data from "./data"
+// import quotes from "./quotes";
+// import posts from "./data/posts";
 import './index.css';
-import StateComponent from "./components/StateComponent";
-import RandomQuote from "./components/RandomQuote";
-import TextAdd from "./components/TextAdd";
-import BlogPost from "./components/BlogPost";
-import Boxes from "./components/Boxes";
-import Search from "./components/Search";
-import ConditionalRendering from "./components/ConditionalRendering";
+// import StateComponent from "./components/StateComponent";
+// import RandomQuote from "./components/RandomQuote";
+// import TextAdd from "./components/TextAdd";
+// import BlogPost from "./components/BlogPost";
+// import Boxes from "./components/Boxes";
+// import Search from "./components/Search";
+// import ConditionalRendering from "./components/ConditionalRendering";
 
 
 
 function App(props) {
 
+    const list = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
     // array of <Card/> element
-    const cards = data.map( prop => {
-        return <Card
-            key={prop.id} 
-            img={prop.img}
-            userName={prop.userName}
-            userWeb={prop.userWeb} 
-        />
-    });
+    // const cards = data.map( prop => {
+    //     return <Card
+    //         key={prop.id} 
+    //         img={prop.img}
+    //         userName={prop.userName}
+    //         userWeb={prop.userWeb} 
+    //     />
+    // });
+
+    // Lists
+    
     return (
         <div>
-            <Navbar/>
-            <MainContent/>
-            <div className="flex">
+            {/* <Navbar/> */}
+            {/* <MainContent/> */}
+            {/* <div className="flex">
             {cards}
-            </div>
+            </div> */}
             {/* <Map/> */}
-            <StateComponent/>
+            {/* <StateComponent/> */}
             {/* <RandomQuote {...quotes}/> */}
-            <TextAdd/>
-            <div className="flex">
+            {/* <TextAdd/> */}
+            {/* <div className="flex">
             <BlogPost post = {posts[0]}/>
-            </div>
-            <Boxes/>
-            <Search className="search-wrapper"/> 
-            <ConditionalRendering/>
+            </div> */}
+            {/* <Boxes/> */}
+            {/* <Search className="search-wrapper"/>  */}
+            {/* <ConditionalRendering/> */}
+            <List list={list}/>
+            <Search/>
+            <Caution type="tags caution"  text="lorem ispkfn fdsf sdfla ape fonf dsd fdkfdf sfpfs fs;fjsff "/>
+            <Caution type="tags warning"  text="lorem ispkfn fdsf sdfla ape fonf dsd fdkfdf sfpfs fs;fjsff "/>
+            <Caution type="tags tip"  text="lorem ispkfn fdsf sdfla ape fonf dsd fdkfdf sfpfs fs;fjsff "/>
         </div>
         
     );
@@ -59,23 +85,37 @@ root.render(
 <App />
 );
 
-// Import the functions you need from the SDKs you need
 
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBlGHnf8V_HiMGOmu5Ql4DxrTCnHukSZFM",
-//     authDomain: "react-facts.firebaseapp.com",
-//     projectId: "react-facts",
-//     storageBucket: "react-facts.appspot.com",
-//     messagingSenderId: "800340411508",
-//     appId: "1:800340411508:web:7f6c36c0631ec839e3fd21",
-//     measurementId: "G-3548QKL94S"
-// };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const List = (props) => 
+    <ul>
+        {
+            props.list.map(listItem => <Item key={listItem.objectID} item={listItem}/>)
+        }
+    </ul>
+
+const Search = () => {
+    // do something in between
+    const handleChnage = (event) => console.log(event.target.value)
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" onChange={handleChnage} />
+        </div>
+    );
+    };
+
+const Item = props => <li>
+    <span>{props.item.title}, </span>
+    <span>{props.item.author}, </span>
+    <strong>{props.item.points}</strong>
+</li>
+
+const Caution = props => 
+<div className={props.type}>
+    {props.type === "tags caution" && <div className="icon">⚠️</div>}  
+    {props.type === "tags warning" && <div className="icon">⛔</div>}
+    {props.type === "tags tip" && <div className="icon">💡</div>}
+    <div className="text">{props.text}</div>
+</div>
